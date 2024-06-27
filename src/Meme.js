32eda77ae328dca2793a81ab5e119303048e871f
Ankit Mod <ankit300mod@gmail.com>
2024-06-27 23:56:13 +0530

    modified

--- src/Meme.js
+++ src/Meme.js
@@ -3,16 +3,16 @@ import "./Meme.css";
 
 const Meme = () => {
     let [gif, setGif] = useState('');
-    
+    let [loading, setLoading] = useState(false);
     let API_KEY = "kC0kZcGTTNZITKMQPLaxGwHeGpwYMn4S";
 
     const handlesubmit = async () => {
-      
+        setLoading(true);
         let URL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
         let api = await fetch(URL);
         let app = await api.json();
         setGif(app.data.images.downsized_large.url);
-       
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -24,16 +24,16 @@ const Meme = () => {
     }
 
     return (
-        <div className='container'>
+        <div className='container'> 
             <div className='heading'>
                 <h1>GIF</h1>
             </div>
             <div className='img'>
-                 <img src={gif} alt="Random GIF" />
+                {loading ? <p>Loading...</p> : <img src={gif} alt="Random GIF" />}
             </div>
             <div className='button'>
-                <button onClick={getapi}>
-                    GENERATE
+                <button onClick={getapi} disabled={loading}>
+                    {loading ? "Loading..." : "GENERATE"}
                 </button>
             </div>
         </div>
